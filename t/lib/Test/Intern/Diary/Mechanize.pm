1f1aff646a59c9492ddf5bc305b737562dbe93a0
tarao <tarao.gnn@gmail.com>
2015-06-18 15:51:02 +0900

    Middleware HTTPExceptions is needed also in tests.

--- t/lib/Test/Intern/Diary/Mechanize.pm
+++ t/lib/Test/Intern/Diary/Mechanize.pm
@@ -5,6 +5,7 @@ use warnings;
 use utf8;
 
 use parent qw(Test::WWW::Mechanize::PSGI);
+use Plack::Builder;
 
 use Test::More ();
 
@@ -12,6 +13,12 @@ use Exporter qw(import);
 our @EXPORT = qw(create_mech);
 
 use Intern::Diary;
+
+my $app = builder {
+    enable 'HTTPExceptions';
+
+    Intern::Diary->as_psgi;
+};
 
 sub create_mech (;%) {
     return __PACKAGE__->new(@_);
@@ -21,7 +28,7 @@ sub new {
     my ($class, %opts) = @_;
 
     my $self = $class->SUPER::new(
-        app     => Intern::Diary->as_psgi,
+        app     => $app,
         %opts,
     );
 
