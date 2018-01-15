7824317b964a1f82bbdf79de32661b147f1a9138
voidsatisfaction <lourie@naver.com>
2018-01-15 17:08:23 +0900

    test: add create article factory

--- t/lib/Test/Intern/Diary/Factory.pm
+++ t/lib/Test/Intern/Diary/Factory.pm
@@ -10,12 +10,14 @@ use Exporter::Lite;
 our @EXPORT = qw(
   create_user
   create_diary
+  create_article
 );
 
 use Intern::Diary::Util;
 use Intern::Diary::Context;
 use Intern::Diary::Service::User;
 use Intern::Diary::Service::Diary;
+use Intern::Diary::Service::Article;
 
 sub create_user {
   my %args = @_;
@@ -53,6 +55,29 @@ sub create_diary {
   return Intern::Diary::Service::Diary->get_diary_by_user_and_title($dbh, +{
     title => $title,
     user => $user,
+  });
+}
+
+sub create_article {
+  my %args = @_;
+
+  my $title = $args{title} // random_regex('test_article_\w{30}');
+  my $body = $args{body} // random_regex('test_article_\w{200}');
+  my $created_at = $args{created_at} // Intern::Diary::Util->now;
+  my $updated_at = $args{updated_at} // Intern::Diary::Util->now;
+  my $diary = $args{diary} // create_diary;
+
+  my $c = Intern::Diary::Context->new;
+  my $dbh = $c->dbh;
+
+  $dbh->query(q[
+    INSERT INTO article (title, body, created_at, updated_at, diary_id)
+      VALUES (?)
+  ], [ $title, $body, $created_at, $updated_at, $diary->id ]);
+
+  return Intern::Diary::Service::Article->get_article_by_diary_and_title($dbh, +{
+    title => $title,
+    diary => $diary,
   });
 }
 
