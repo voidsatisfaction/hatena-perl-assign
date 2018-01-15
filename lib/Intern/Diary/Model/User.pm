813fdfd3c24023859d9fdc05f67888902a4284b5
voidsatisfaction <lourie@naver.com>
2018-01-15 15:44:26 +0900

    refactor: add user model

--- lib/Intern/Diary/Model/User.pm
+++ lib/Intern/Diary/Model/User.pm
@@ -4,10 +4,10 @@ use strict;
 use warnings;
 use utf8;
 
-use Data::Dumper;
-
 use DateTime;
 use DateTime::Format::MySQL;
+
+use Intern::Diary::Util;
 
 use Class::Accessor::Lite (
   ro => [qw(
@@ -17,15 +17,12 @@ use Class::Accessor::Lite (
   new => 1,
 );
 
-# QUESTION: What is the meaning of this method?
-# Just for cache?
 sub created_at {
   my ($self) = @_;
   $self->{_created_at} ||= eval {
-    my $dt = DateTime::Format::MySQL->parse_datetime( $self->{created_at} );
-    $dt -> set_time_zone('UTC');
-    $dt -> set_formatter( DateTime::Format::MySQL->new );
-    $dt;
+    Intern::Diary::Util::datetime_from_db(
+      $self->{created_at}
+    );
   };
 }
 
