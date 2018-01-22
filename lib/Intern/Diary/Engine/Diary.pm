288bfc4edee2172b1d0bb919dfe8a1d3430bb629
voidsatisfaction <lourie@naver.com>
2018-01-22 18:29:30 +0900

    fix: change user diaries get private to public

--- lib/Intern/Diary/Engine/Diary.pm
+++ lib/Intern/Diary/Engine/Diary.pm
@@ -6,14 +6,19 @@ use utf8;
 
 use Intern::Diary::Context;
 
+use Intern::Diary::Service::User;
 use Intern::Diary::Service::Diary;
 
 sub user_diaries_get {
   my ($class, $c) = @_;
-  $c->check_signin_and_redirect;
+
+  my $user_name = $c->req->parameters->{username};
+  my $user = Intern::Diary::Service::User->get_user_by_name($c->dbh, +{
+    name => $user_name,
+  });
 
   my $diaries = Intern::Diary::Service::Diary->get_diaries_by_user($c->dbh, +{
-    user => $c->user,
+    user => $user,
   });
 
   $c->html('diary/user_diaries_get.html', +{
