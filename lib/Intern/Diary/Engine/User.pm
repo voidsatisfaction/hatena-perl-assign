d1afafae4c15f4cfa25afc6f82d86d1fb59c4931
voidsatisfaction <lourie@naver.com>
2018-01-23 12:39:29 +0900

    test: add User controller tests

--- lib/Intern/Diary/Engine/User.pm
+++ lib/Intern/Diary/Engine/User.pm
@@ -6,6 +6,8 @@ use utf8;
 
 use DateTime;
 
+use Intern::Diary::Service::User;
+
 sub signin_get {
   my ($class, $c) = @_;
 
@@ -16,6 +18,10 @@ sub signin_post {
   my ($class, $c) = @_;
 
   my $user_name = $c->req->parameters->{username};
+
+  Intern::Diary::Service::User->get_or_create_by_name($c->dbh, +{
+    name => $user_name,
+  });
 
   # TODO: not use cookie, use session
   $c->set_cookie(+{
