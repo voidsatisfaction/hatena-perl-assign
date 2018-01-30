f00139f45bff62afeaf96cccd1f210fc9fb29cde
voidsatisfaction <lourie@naver.com>
2018-01-30 18:27:11 +0900

    chore: add seed data

--- db/seed.sql
+++ db/seed.sql
@@ -10,3 +10,13 @@ INSERT INTO article (diary_id, title, body, created_at, updated_at) VALUES (1, '
 INSERT INTO article (diary_id, title, body, created_at, updated_at) VALUES (1, 'monster2', 'hello monster222', '2008-01-10', '2017-01-01');
 INSERT INTO article (diary_id, title, body, created_at, updated_at) VALUES (2, 'monster3', 'hello monster3', '2009-01-10', '2017-01-01');
 INSERT INTO article (diary_id, title, body, created_at, updated_at) VALUES (3, 'monster22312', 'hello monster33', '2010-01-10', '2017-01-01');
+INSERT INTO article (diary_id, title, body, created_at, updated_at) VALUES (1, 'monster123', 'hello monster222', '2008-01-10', '2017-01-01');
+INSERT INTO article (diary_id, title, body, created_at, updated_at) VALUES (1, 'monsteasdfar2', 'hello monster222', '2008-01-10', '2017-01-01');
+INSERT INTO article (diary_id, title, body, created_at, updated_at) VALUES (1, 'mons123ter22', 'hello monster222', '2008-01-10', '2017-01-01');
+INSERT INTO article (diary_id, title, body, created_at, updated_at) VALUES (1, 'monstwer2', 'hello monster222', '2008-01-10', '2017-01-01');
+INSERT INTO article (diary_id, title, body, created_at, updated_at) VALUES (1, 'monster22312stwer2', 'hello monster222', '2008-01-10', '2017-01-01');
+INSERT INTO article (diary_id, title, body, created_at, updated_at) VALUES (1, 'mons1t er2', 'hello monster222', '2008-01-10', '2017-01-01');
+INSERT INTO article (diary_id, title, body, created_at, updated_at) VALUES (1, 'mons1231123ter2', 'hello monster222', '2008-01-10', '2017-01-01');
+INSERT INTO article (diary_id, title, body, created_at, updated_at) VALUES (1, 'mons3ter2', 'hello monster222', '2008-01-10', '2017-01-01');
+INSERT INTO article (diary_id, title, body, created_at, updated_at) VALUES (1, 'mons4ter2', 'hello monster222', '2008-01-10', '2017-01-01');
+INSERT INTO article (diary_id, title, body, created_at, updated_at) VALUES (1, 'mons5ter2', 'hello monster222', '2008-01-10', '2017-01-01');
