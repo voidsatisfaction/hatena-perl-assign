287834079b749057c8547c054080a40568ee74ad
voidsatisfaction <lourie@naver.com>
2018-02-08 11:11:32 +0900

    feat: add all_article_with_pagination_api

--- lib/Intern/Diary/Engine/API/Article.pm
+++ lib/Intern/Diary/Engine/API/Article.pm
@@ -9,16 +9,22 @@ use Intern::Diary::Service::Article;
 sub all_articles_with_pagination {
   my ($class, $c) = @_;
 
-  # TODO: make pagination setting
+  my $page = $c->req->parameters->{page};
+  my $per_page = $c->req->parameters->{per_page} // $c->default_per_page;
 
   my $articles = Intern::Diary::Service::Article->get_articles($c->dbh, +{
-    per_page => 10,
-    page => 0,
+    per_page => $per_page,
+    page => $page,
+    order_by => 'created_at DESC'
   });
 
   my $json_articles = [ map { $_->json_hash } @$articles ];
 
-  $c->json($json_articles);
+  $c->json(+{
+    articles => $json_articles,
+    per_page => $per_page,
+    next_page => $page + 1,
+  });
 }
 
 1;
