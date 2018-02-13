208f532a7c82394705df9ce5d3599140818a5879
voidsatisfaction <lourie@naver.com>
2018-02-13 16:18:11 +0900

    feat: beautify mouse Stalker

--- mouseStalker/src/main.js
+++ mouseStalker/src/main.js
@@ -1,15 +1,31 @@
-import mouseStalker from './mouseStalker/mouseStalker';
+export default class MouseStalker {
+  constructor(opts) {
+    const { imageUrl, width, height } = opts;
+    const $stalker = document.createElement('div');
+    $stalker.setAttribute('id', 'mouse-stalker-element');
+    $stalker.style.position = 'absolute';
+    $stalker.style.display = 'inline-block';
+    $stalker.style.width = `${width || '30px'}`;
+    $stalker.style.height = `${height || '30px'}`;
+    if (imageUrl) {
+      $stalker.style.backgroundImage = `url(${imageUrl})`;
+    } else {
+      $stalker.style.backgroundColor = "red";
+    }
+    $stalker.style.backgroundSize = 'cover';
+    $stalker.style.backgroundRepeat = 'no-repeat';
+    document.body.append($stalker);
 
-import './main.css';
+    document.addEventListener('mousemove', (e) => {
+      const x = `${e.x + 15}px`;
+      const y = `${e.y + 15}px`;
+      $stalker.style.left = x;
+      $stalker.style.top = y;
+    });
 
-document.addEventListener('mousemove', (e) => {
-  const stalker = document.querySelector('#stalker');
-  const x = `${e.x}px`;
-  const y = `${e.y}px`;
-  stalker.style.left = x;
-  stalker.style.top = y;
-});
-
+    this.$element = $stalker;
+  }
+}
 
 if (module.hot) {
   module.hot.accept();
