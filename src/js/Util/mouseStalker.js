39e0bc30a67e145459a9c016b47f723a027f2302
voidsatisfaction <lourie@naver.com>
2018-02-13 16:22:35 +0900

    feat: apply mouse stalker on Intern::Diary

---
+++ src/js/Util/mouseStalker.js
@@ -0,0 +1,28 @@
+export default class MouseStalker {
+  constructor(opts) {
+    const { imageUrl, width, height } = opts;
+    const $stalker = document.createElement('div');
+    $stalker.setAttribute('id', 'mouse-stalker-element');
+    $stalker.style.position = 'absolute';
+    $stalker.style.display = 'inline-block';
+    $stalker.style.width = `${width || '30px'}`;
+    $stalker.style.height = `${height || '30px'}`;
+    if (imageUrl) {
+      $stalker.style.backgroundImage = `url(${imageUrl})`;
+    } else {
+      $stalker.style.backgroundColor = "red";
+    }
+    $stalker.style.backgroundSize = 'cover';
+    $stalker.style.backgroundRepeat = 'no-repeat';
+    document.body.append($stalker);
+
+    document.addEventListener('mousemove', (e) => {
+      const x = `${e.x + 15}px`;
+      const y = `${e.y + 15}px`;
+      $stalker.style.left = x;
+      $stalker.style.top = y;
+    });
+
+    this.$element = $stalker;
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
 import ArticlesController from './Controller/articles';
+import MouseStalker from './Util/mouseStalker';
 
 (function() {
-  const $articlesContainer = document.querySelector('.articles-container');
+  const $mouseStalker = new MouseStalker({
+    width: '50px',
+    height: '50px',
+    imageUrl: 'https://cdn.worldvectorlogo.com/logos/gopher.svg',
+  });
+
+  const $articlesContainer = document.querySelector('.articles-container') || undefined;
   const articlesController = new ArticlesController($articlesContainer);
 })();
